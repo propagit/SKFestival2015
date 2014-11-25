244008ea1028d1def494b03adacfbc83e4b74124
Kaushtuv <kaushtuvgurung@gmail.com>
2014-11-26 10:47:01 +1100

    mob menu

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -58,45 +58,9 @@
                     <input type="text" id="head-subscribe-input" placeholder="SUBSCRIBE">
                		</div>
                     
-                	<div class="social_icons span6 remove-gutters pull hidden-phone">
-                    <ul class="nav" id="top-navigation">
-                        <li>
-                        	<a href="https://www.facebook.com/StKildaFestival" target="_blank">
-                        		<span class="fa-stack">
-                                	<i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-facebook fa-stack-1x txt-white"></i>
-                                </span>
-                        	</a>
-                        </li>
-                        <li>
-                       		 <a href="http://twitter.com/stkildafestival" target="_blank">
-                            	<span class="fa-stack">
-                                	<i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x txt-white"></i>
-                                </span>
-                             </a>
-                        </li>
-                        <li>
-                       		 <a href="http://www.youtube.com/thestkildafestival" target="_blank">
-                            	<span class="fa-stack">
-                                	<i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-youtube fa-stack-1x txt-white"></i>
-                                </span>
-                             </a>
-                        </li>
-                        <li>
-                       		 <a href="#" target="_blank">
-                            	<span class="fa-stack">
-                                	<i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-instagram fa-stack-1x txt-white"></i>
-                                </span>
-                             </a>
-                        </li>
-                        <li class="hidden-phone">
-                            <input type="text" id="search-input" placeholder="search"/>
-                        </li>
-                    </ul>
-                </div>
+                	<div class="social_icons span6 remove-gutters pull hidden-phone" id="top-navigation">
+                        <?php $this->load->view('common/social_icons');?>
+                	</div>
                 </div>
 			</div>
 		</div>
--- application/views/common/mob_nav.php
+++ application/views/common/mob_nav.php
@@ -67,61 +67,75 @@ $(function(){
 <div id="menu-for-phone">
 	<i class="fa fa-bars mob-nav-bars" id="toggle-nav" data-toggle="mob-nav"></i>
 	
-    <div class="container header-menu-phone hide" id="mob-nav">
-    	<?php
-	        $menus = $this->Menu_model->get_menus();
-    	    $total_menus = count($menus);
-        	$i = 1;
-			foreach($menus as $menu)
-			{
-				$links = $this->Menu_model->get_links($menu['id'],0);
-				$plus = '';    
-				if(count($links) > 0)
-				{
-					$plus = '+';
-				}
-				if(count($links) > 0)
-				{
-					?>
-						<button onclick="show_sub(<?=$menu['id']?>)" class="top-menu">
-				    		<?=$menu['name']?> <span id="sign<?=$menu['id']?>"><?=$plus?></span>
-				    	</button>
-					<?
-				}
-				else 
-				{
-					?>
-						<button onclick="window.location='<?=base_url()?>page/pages/<?=$menu['url']?>'" class="top-menu">
-				    		<?=$menu['name']?> <span id="sign<?=$menu['id']?>"><?=$plus?></span>
-				    	</button>
-					<?
-				}
-				if(count($links) > 0)
-				{
-				?>
-				<div id="subtop-menu-wrapper<?=$menu['id']?>" style="display: none;">
-					<?php
-					foreach($links as $link)
-					{
-						$url = '#';
-                        if($link['url'] != ''){
-                            if(is_numeric($link['url'])){
-                                $url = base_url().'page/pages/'.$link['url'];
-                            }else{
-                                $url = $link['url'];
-                            }
-                        }
-					?>
-					<button onclick="window.location='<?=$url?>'" class="subtop-menu">
-			    		<?=$link['name']?>
-			    	</button>
-					<?
-					}
-					?>
-				</div>
-				<?
-				}
-			}
-		?>
-	</div><!--container-->
+    <div id="mob-nav" class="hide">
+     	<div class="mob-search-box">
+          <input type="text" class="mob-search-input" placeholder="SEARCH">
+        </div>
+        
+        <div class="container header-menu-phone">
+        <?php
+          $menus = $this->Menu_model->get_menus();
+          $total_menus = count($menus);
+          $i = 1;
+          foreach($menus as $menu)
+          {
+              $links = $this->Menu_model->get_links($menu['id'],0);
+              $plus = '';    
+              if(count($links) > 0)
+              {
+                  $plus = '+';
+              }
+              if(count($links) > 0)
+              {
+                  ?>
+                      <div onclick="show_sub(<?=$menu['id']?>)" class="top-menu">
+                          <?=$menu['name']?> <span id="sign<?=$menu['id']?>"><?=$plus?></span>
+                      </div>
+                  <?
+              }
+              else 
+              {
+                  ?>
+                      <div onclick="window.location='<?=base_url()?>page/pages/<?=$menu['url']?>'" class="top-menu">
+                          <?=$menu['name']?> <span id="sign<?=$menu['id']?>"><?=$plus?></span>
+                      </div>
+                  <?
+              }
+              if(count($links) > 0)
+              {
+              ?>
+              <div id="subtop-menu-wrapper<?=$menu['id']?>" style="display: none;" class="mob-subnav">
+                  <?php
+                  foreach($links as $link)
+                  {
+                      $url = '#';
+                      if($link['url'] != ''){
+                          if(is_numeric($link['url'])){
+                              $url = base_url().'page/pages/'.$link['url'];
+                          }else{
+                              $url = $link['url'];
+                          }
+                      }
+                  ?>
+                  <div onclick="window.location='<?=$url?>'" class="subtop-menu">
+                      <?=$link['name']?>
+                  </div>
+                  <?
+                  }
+                  ?>
+              </div>
+              <?
+              }
+          }
+        ?>
+        
+        
+        <div class="social_icons span6 remove-gutters mob-social">
+        <?php $this->load->view('common/social_icons');?>
+        </div>    
+        
+        
+        </div><!--container-->
+    </div><!--mob-nav-->
+
 </div>
